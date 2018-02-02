c2ac5f826b0d5004fddc275578dc6e0855d8d7fb
Thomas Ernst <thomas@thernst.de>
2018-02-02 06:12:15 +0100

    [Task] Make usage of self.get_parameter_value (#18)
    parameters have been added to plugin.yaml

--- __init__.py
+++ __init__.py
@@ -31,46 +31,29 @@ from lib.model.smartplugin import SmartPlugin
 
 
 class AutoBlind(SmartPlugin):
-    ALLOW_MULTIINSTANCE = False
     PLUGIN_VERSION = '1.4.0'
 
     # Constructor
-    # smarthome: instance of smarthome.py
-    # startup_delay_default: default startup delay
-    # manual_break_default: default break after manual changes of items
-    # log_level: loglevel for extended logging
-    # log_directory: directory for extended logging files
-    def __init__(self,
-                 smarthome,
-                 startup_delay_default=10,
-                 suspend_time_default=3600,
-                 manual_break_default=0,
-                 log_level=0,
-                 log_directory="var/log/AutoBlind/",
-                 log_maxage="0",
-                 laststate_name_manually_locked="Manuell gesperrt",
-                 laststate_name_suspended="Ausgesetzt bis %X"):
+    # noinspection PyUnusedLocal,PyMissingConstructor
+    def __init__(self, sh, *args, **kwargs):
 
         self.logger = logging.getLogger(__name__)
         self.__items = {}
         self.alive = False
         self.__cli = None
 
+        log_level = self.get_parameter_value("log_level")
+        log_directory = self.get_parameter_value("log_directory")
         self.logger.info("Init AutoBlind (log_level={0}, log_directory={1})".format(log_level, log_directory))
 
-        AutoBlindDefaults.startup_delay = int(startup_delay_default)
-        AutoBlindDefaults.suspend_time = int(suspend_time_default)
-        AutoBlindDefaults.laststate_name_manually_locked = laststate_name_manually_locked
-        AutoBlindDefaults.laststate_name_suspended = laststate_name_suspended
+        AutoBlindDefaults.startup_delay = self.get_parameter_value("startup_delay_default")
+        AutoBlindDefaults.suspend_time = self.get_parameter_value("suspend_time_default")
+        AutoBlindDefaults.laststate_name_manually_locked = self.get_parameter_value("laststate_name_manually_locked")
+        AutoBlindDefaults.laststate_name_suspended = self.get_parameter_value("laststate_name_suspended")
         AutoBlindDefaults.write_to_log()
 
-        if manual_break_default != 0:
-            text = "Using obsolete plugin config attribute 'manual_break_default'. Change to 'suspend_time_default'!"
-            self.logger.warning(text)
+        AutoBlindCurrent.init(self.get_sh())
 
-        AutoBlindCurrent.init(smarthome)
-
-        log_level = AutoBlindTools.cast_num(log_level)
         if log_level > 0:
             if log_directory[0] != "/":
                 base = self.get_sh().get_basedir()
@@ -83,14 +66,14 @@ class AutoBlind(SmartPlugin):
             AbLogger.set_logdirectory(log_directory)
             text = "AutoBlind extended logging is active. Logging to '{0}' with loglevel {1}."
             self.logger.info(text.format(log_directory, log_level))
-        log_maxage = AutoBlindTools.cast_num(log_maxage)
+        log_maxage = self.get_parameter_value("log_maxage")
         if log_level > 0 and log_maxage > 0:
             self.logger.info("AutoBlind extended log files will be deleted after {0} days.".format(log_maxage))
             AbLogger.set_logmaxage(log_maxage)
             cron = ['init', '30 0 * *']
-            self.get_sh().scheduler.add('AutoBlind: Remove old logfiles', AbLogger.remove_old_logfiles, cron=cron, offset=0)
+            self.scheduler_add('AutoBlind: Remove old logfiles', AbLogger.remove_old_logfiles, cron=cron, offset=0)
 
-        smarthome.autoblind_plugin_functions = AutoBlindFunctions.AbFunctions(self.get_sh())
+            self.get_sh().autoblind_plugin_functions = AutoBlindFunctions.AbFunctions(self.get_sh())
 
     # Parse an item
     # noinspection PyMethodMayBeStatic
@@ -137,7 +120,7 @@ class AutoBlind(SmartPlugin):
         for entry in changed_by:
             entry_caller, __, entry_source = entry.partition(":")
             if (entry_caller == original_caller or entry_caller == "*") and (
-                    entry_source == original_source or entry_source == "*"):
+                            entry_source == original_source or entry_source == "*"):
                 return True
         return False
 
@@ -150,6 +133,6 @@ class AutoBlind(SmartPlugin):
         for entry in changed_by:
             entry_caller, __, entry_source = entry.partition(":")
             if (entry_caller == original_caller or entry_caller == "*") and (
-                    entry_source == original_source or entry_source == "*"):
+                            entry_source == original_source or entry_source == "*"):
                 return False
         return True
